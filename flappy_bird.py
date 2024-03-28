916f72cb18f94e9a2b19194b840f4ae1dc502213
yzhang711118 <55716106+yzhang711118@users.noreply.github.com>
2024-03-27 21:12:59 -0400

    Update flappy_bird.py

--- flappy_bird.py
+++ flappy_bird.py
@@ -2,8 +2,8 @@
 The classic game of flappy bird. Make with python
 and pygame. Features pixel perfect collision using masks :o
 
-Date Modified:  Jul 30, 2019
-Author: Tech With Tim
+Date Modified:  June 6, 2020
+Author: harlilily
 Estimated Work Time: 5 hours (1 just for that damn collision)
 """
 import pygame
